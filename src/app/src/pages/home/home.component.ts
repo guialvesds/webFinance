281a9aaa5c33a1d0ac5508568bfb4b68279031bf
guialvesds <guilherme_ealves@outlook.com>
2025-04-11 22:51:30 -0300

    SidNav implemented

--- src/app/src/pages/home/home.component.ts
+++ src/app/src/pages/home/home.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @Component({
   selector: 'app-home',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
 
 export class HomeComponent {
+
+  // showFiller = true;
 }
