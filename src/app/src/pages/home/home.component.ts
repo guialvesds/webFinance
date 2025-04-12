390c614c663336a63683761273bf608585fd8bec
guialvesds <guilherme_ealves@outlook.com>
2025-04-11 22:18:29 -0300

    Add toolbar in home page

--- src/app/src/pages/home/home.component.ts
+++ src/app/src/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
